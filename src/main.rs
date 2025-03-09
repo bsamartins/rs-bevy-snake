548a0f8e79a85119c762980ba7b4fd478e714365
Bernardo Martins <bsamartins@gmail.com>
2025-03-09 23:09:37 +0000

    Add snake head

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,43 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_systems(Startup, setup_camera)
+        .add_systems(Startup, spawn_snake)
+        .add_systems(Update, snake_movement)
         .add_plugins(DefaultPlugins)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn_batch(vec![Camera2d::default()]);
+    commands.spawn(Camera2d);
+}
+
+const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
+
+#[derive(Component)]
+struct SnakeHead;
+
+fn spawn_snake(mut commands: Commands) {
+    commands.spawn(
+        Sprite::from_color(SNAKE_HEAD_COLOR, Vec2::new(10.0, 10.0)),
+    ).insert(SnakeHead);
+}
+
+fn snake_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+) {
+    for mut transform in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            transform.translation.y += 2.;
+        }
+    }
 }
